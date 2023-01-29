30bcd0bbc8ceb4a1a75a24f1aab3dcbdfd134bf4
Minhaj Islam <minhajislam674@gmail.com>
2023-01-29 20:17:25 +0100

    src/--

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FetchApiDataService {
+
+  constructor() { }
+}
